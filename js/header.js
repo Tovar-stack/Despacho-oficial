ba03faa69ac46276a2b237687d961ae43dc96f40
Robert Tovar <57671052+Tovar-stack@users.noreply.github.com>
2020-04-21 04:52:33 -0500

    update

--- js/header.js
+++ js/header.js
@@ -1,6 +1,7 @@
 jQuery('document').ready(function($){
 	var menuBtn = $('.menu-icon'),
 	menu = $('.navigation ul');
+	menu2 = $('.logo img')
 
 	menuBtn.click(function(){
 
@@ -9,6 +10,13 @@ jQuery('document').ready(function($){
 
 		}else{
 			menu.addClass('show')
+		}
+
+		if (menu2.hasClass('show')) {
+			menu2.removeClass('show');
+
+		}else{
+			menu2.addClass('show')
 		}
 	})
 });
